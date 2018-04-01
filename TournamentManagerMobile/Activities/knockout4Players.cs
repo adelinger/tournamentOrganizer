70d814903176c8f90800728eb4879d6667aec7f3
adelinger <31591846+adelinger@users.noreply.github.com>
2018-04-01 11:57:26 +0200

    Added 4playersKnockout layout and activity

---
+++ TournamentManagerMobile/Activities/knockout4Players.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace TournamentManagerMobile.Activities
+{
+    [Activity(Label = "knockout4Players")]
+    public class knockout4Players : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.knockout4Players);
+        }
+    }
+}

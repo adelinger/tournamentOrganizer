6da696faee4178edb9e3a171f2c24de136f5da79
adelinger <31591846+adelinger@users.noreply.github.com>
2018-09-11 19:54:12 +0200

    started knockoutType

--- TournamentManagerMobile/Activities/knockout4Players.cs
+++ TournamentManagerMobile/Activities/knockout4Players.cs
@@ -15,11 +15,34 @@ namespace TournamentManagerMobile.Activities
     [Activity(Label = "knockout4Players")]
     public class knockout4Players : Activity
     {
+        private Button incrementOne;
+        private Button incrementTwo;
+        private Button decrementOne;
+        private Button decrementTwo;
+        private Button chooseMatch;
+        private Button applyResult;
+        private Button knockOutDraw;
+        private Button goalScorers;
+
+        private TextView playerOne;
+        private TextView playerTwo;
+
+        private EditText resultOne;
+        private EditText resultTwo;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             SetContentView(Resource.Layout.knockout4Players);
+
+            incrementOne = FindViewById<Button>(Resource.Id.plusIncrementKnockout);
+            incrementTwo = FindViewById<Button>(Resource.Id.plusIncrementP2Knockout);
+            decrementOne = FindViewById<Button>(Resource.Id.minusIncrementKnockout);
+            decrementTwo = FindViewById<Button>(Resource.Id.minusIncrementP2Knockout);
+            chooseMatch = FindViewById<Button>(Resource.Id.nextMatchKnockout);
+            applyResult = FindViewById<Button>(Resource.Id.applyButtonKnockout);
+
         }
     }
 }
